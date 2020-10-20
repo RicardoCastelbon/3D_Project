5386dc86f90ca69cbd1f328fdd1227843a22562b
Ricardo Castelbon <62264856+AyzenX@users.noreply.github.com>
2020-10-20 17:33:48 +0200

    Project updated

---
+++ AncientCliff_Project/Assets/Prefaps/cambioCamara.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cambioCamara : MonoBehaviour
+{
+
+    public GameObject camaraAnimada;
+    public GameObject controlador;
+
+    // Use this for initialization
+    void Start()
+    {
+        controlador.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            camaraAnimada.SetActive(true);
+            controlador.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlador.SetActive(true);
+            camaraAnimada.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
